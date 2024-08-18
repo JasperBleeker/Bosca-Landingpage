6454a23bd010c8c49df1d4a24a3e27a9a4611f03
JasperBleeker <jasper.bleeker@stud.fhgr.ch>
2024-08-18 18:06:28 +0200

    Navigation rebuild
    -Static Navigation
    -Footer Logo moved to own file for better readability

--- frontend/components/NavBar.vue
+++ frontend/components/NavBar.vue
@@ -1,31 +1,34 @@
 <template>
     <div>
-      <nav v-if="menuItems.length">
+      <nav>
         
-        <ul class="flex flex-col items-center justify-end gap-24 text-center md:flex-row">
-          <li v-for="item in menuItems" :key="item.id" class="font-body font-bold text-2xl text-white">
-            <nuxt-link :to="item.attributes.MenuOptionen.link">
-              {{ item.attributes.Name }}
-            </nuxt-link>
+        <ul class="font-body font-bold text-2xl text-white flex flex-col items-center justify-end gap-24 text-center md:flex-row">
+          <li>
+            <NuxtLink to="/">Home</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/haus">Haus</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/standort">Standort</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/ueber">Über Mich</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/kontakt">Kontakt</NuxtLink>
           </li>
         </ul>
       </nav>
   
-      <div v-else-if="error">Error: {{ error.message }}</div>
-      <div v-else>Loading...</div>
+      
     </div>
   </template>
   
   <script setup lang="ts">
-  import { computed } from 'vue'
+
+
   
-  // Fetching navigation items
-  const { data: navigationItems, error } = await useFetch('/api/strapi-fetch')
-  
-  // Computed property to filter items for the main menu
-  const menuItems = computed(() =>
-    navigationItems.value?.filter(item => item.attributes.MenuOptionen.menu) || []
-  )
   </script>
   
   <style scoped>
