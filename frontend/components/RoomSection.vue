4fd4580663c919f53f0533341b93be72731ab5f5
JasperBleeker <jasper.bleeker@stud.fhgr.ch>
2024-08-23 15:21:26 +0200

    Rooms gallery improvement
    -Prettier grid
    
    To do:
    Fullscreen image on click with slideshow

--- frontend/components/RoomSection.vue
+++ frontend/components/RoomSection.vue
@@ -2,10 +2,15 @@
     <div v-if="roomsData && roomsData.length > 0" class="text-moss bg-white">
         <div v-for="room in roomsData" :key="room.id" class="pb-8">
             <h2 class="font-heading-3 text-center text-3xl font-bold mb-6 pt-40">{{ room.attributes.title }}</h2>
-            <div v-if="room.attributes.pictures.data.length > 0"  :class="getGridClass(room.attributes.pictures.data.length)">
-                <div v-for="(picture, index) in room.attributes.pictures.data" :key="index" :class="getImageClass(index, room.attributes.pictures.data.length)">
-                    <img :src="`${baseURL}${picture.attributes.formats.medium.url}`" :alt="picture.attributes.name"
-                        class="w-full h-auto object-cover">
+            <div v-if="room.attributes.pictures.data.length > 0" class="m-auto grid grid-cols-2 gap-4 max-w-2xl grid-auto-rows-min">
+                <div 
+                    v-for="(picture, index) in room.attributes.pictures.data" 
+                    :key="index" 
+                    :class="getImageClass(picture, index, room.attributes.pictures.data)">
+                    <img 
+                        :src="`${baseURL}${picture.attributes.formats.medium.url}`" 
+                        :alt="picture.attributes.name"
+                        class="w-full h-full object-cover">
                 </div>
             </div>
             <div v-else>
@@ -24,6 +29,8 @@ import { useRuntimeConfig, useFetch } from '#app';
 
 interface PictureFormat {
     url: string;
+    width: number;
+    height: number;
 }
 
 interface RoomPicturesAttributes {
@@ -68,26 +75,29 @@ if (error.value) {
     console.error('Room data is empty or not in expected format');
 }
 
-// Function to determine the grid class based on the number of images
-const getGridClass = (imageCount: number) => {
-    return imageCount === 1 ? 'grid grid-cols-1' : 'grid grid-cols-2 gap-4';
+// Function to determine if the image is portrait or landscape based on aspect ratio
+const isPortrait = (picture: RoomPicturesAttributes) => {
+    return picture.formats.medium.height > picture.formats.medium.width;
 };
 
-// Function to determine if an image should span across two columns
-const getImageClass = (index: number, imageCount: number) => {
-    if (imageCount === 1) {
-        return 'col-span-1';
+// Function to determine the appropriate class based on image aspect ratio and its position
+const getImageClass = (picture: { attributes: RoomPicturesAttributes }, index: number, pictures: Array<{ attributes: RoomPicturesAttributes }>) => {
+    const portrait = isPortrait(picture.attributes);
+
+    // Create variations by index or randomly for a dynamic layout
+    if (index === 0 || (index % 4 === 0 && !portrait)) {
+        return 'col-span-2 row-span-2 aspect-w-2 aspect-h-1 relative';
     }
-    if (imageCount === 2) {
-        return index === 0 ? 'col-span-2' : 'col-span-1';
+
+    if (index % 2 === 0) {
+        return 'col-span-1 row-span-2 aspect-w-1 aspect-h-2 relative';
     }
-    if (imageCount > 2) {
-        return index === 0 ? 'col-span-2' : 'col-span-1';
-    }
-    return '';
+
+    return 'col-span-1 aspect-w-1 aspect-h-1 relative';
 };
+
 </script>
 
 <style scoped>
-/* Add any custom styling here */
+/* Add any custom styling here if necessary */
 </style>
