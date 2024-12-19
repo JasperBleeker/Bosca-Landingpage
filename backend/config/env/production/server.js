cca8e528e9c226371ce437e796131eb18d3a9d07
JasperBleeker <jasper.bleeker@stud.fhgr.ch>
2024-12-19 19:02:17 +0100

    bugfix

--- backend/config/env/production/server.js
+++ backend/config/env/production/server.js
@@ -1,5 +1,5 @@
-// path: /config/env/production/server.ts
-export default ({ env }) => ({
+// path: /config/env/production/server.js
+module.exports = ({ env }) => ({
     proxy: true,
     url: env('APP_URL'), // Sets the public URL of the application.
     app: { 
