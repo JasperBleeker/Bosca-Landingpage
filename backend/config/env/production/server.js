25c9206b063abe9e87ac8edc103e503d0ea605ae
JasperBleeker <jasper.bleeker@stud.fhgr.ch>
2024-12-21 21:18:52 +0100

    Update server.js

--- backend/config/env/production/server.js
+++ backend/config/env/production/server.js
@@ -1,8 +1,11 @@
 // path: /config/env/production/server.js
 module.exports = ({ env }) => ({
     proxy: true,
-    url: env('APP_URL') + '/api', // Sets the public URL of the application.
-    app: { 
-      keys: env.array('APP_KEYS')
+    // NOTE: Make sure APP_URL is just the domain, like "https://bosca-9sijs.ondigitalocean.app"
+    // Then we append '/bosca-landingpage-backend' so Strapi knows its real subpath
+    url: env('APP_URL') + '/bosca-landingpage-backend',
+    app: {
+      keys: env.array('APP_KEYS'),
     },
-});
+  });
+  
