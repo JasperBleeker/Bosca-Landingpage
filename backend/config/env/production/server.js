0837b9cb2f0085c365f59dd741baaa74f8c0c484
JasperBleeker <jasper.bleeker@stud.fhgr.ch>
2024-12-21 20:43:51 +0100

    Update server.js

--- backend/config/env/production/server.js
+++ backend/config/env/production/server.js
@@ -1,7 +1,7 @@
 // path: /config/env/production/server.js
 module.exports = ({ env }) => ({
     proxy: true,
-    url: env('APP_URL'), // Sets the public URL of the application.
+    url: env('APP_URL') + '/api', // Sets the public URL of the application.
     app: { 
       keys: env.array('APP_KEYS')
     },
